1d1feb11ca0ae476fc318f95b332406702215a2d
Sean <cancels1@mymail.nku.edu>
2024-10-07 13:09:56 -0400

    Sean's Code

--- README.md
+++ README.md
@@ -4,7 +4,9 @@
 - edited the index.php to display correct company info
 
 ## Sean Cancel
-- 
+- Edit the read_plain_text inside the folder
+- Make the function to read the data
+- Then call the function to read data in index.php
 
 ## Camden Bohanan
 - 
---
+++ lib/read_plain_text.php
@@ -0,0 +1,22 @@
+<?php
+function readPlainTxt($filename) {
+    if (!file_exists($filename) || !is_readable($filename)) {
+        return [];
+    }
+
+    $data = [];
+    if (($handle = fopen($filename, 'r')) !== false) {
+        while (($line = fgets($handle)) !== false) {
+            $data[] = trim($line);
+        }
+        fclose($handle);
+    }
+
+    return $data;
+}
+
+// Call the function in index.php
+$plainTxtData = readPlainTxt('read_plaintxt');
+
+
+?>
